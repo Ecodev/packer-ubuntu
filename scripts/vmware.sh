3870b08eecc602bc58ce21c14d813c5890335b2c
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2015-09-15 11:37:04 +0200

    Release 0.2 - Change ownership of VMware Fusion shared folder - Publish Virtualbox image on Atlas

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -23,3 +23,9 @@ rm -fr /tmp/vmware-tools-distrib
 # Ensure that VMWare Tools recompiles kernel modules when we update the linux images
 sed -i.bak 's/answer AUTO_KMODS_ENABLED_ANSWER no/answer AUTO_KMODS_ENABLED_ANSWER yes/g' /etc/vmware-tools/locations
 sed -i.bak 's/answer AUTO_KMODS_ENABLED no/answer AUTO_KMODS_ENABLED yes/g' /etc/vmware-tools/locations
+
+# Change ownership of VMware Fusion shared folder
+# @see http://askubuntu.com/questions/377029/change-ownership-of-vmware-fusion-shared-folder
+sed -i.bak '\|vmhgfs_mnt="/mnt/hgfs"|a vmuser=${VMWARE_MNT_USER:-root}' /etc/vmware-tools/services.sh
+sed -i.bak 's|vmware_exec_selinux "mount -t vmhgfs .host:/ $vmhgfs_mnt"|uid=id --user $vmuser\n      gid=id --group $vmuser\n      vmware_exec_selinux "mount -t vmhgfs .host:/ $vmhgfs_mnt -o uid=$uid,gid=$gid"|' /etc/vmware-tools/services.sh
+sed -i.bak '\|pre-start exec /etc/vmware-tools/services.sh start|i VMWARE_MNT_USER="root"' /etc/init/vmware-tools.conf
