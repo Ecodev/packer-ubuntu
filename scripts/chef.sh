cc313bf92e5a38049059e9622385d27fbb9bc311
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2015-10-21 15:46:10 +0200

    Release 0.3 - Move the box to an Ecodev organisational atlas account - Set perms on /etc/chef dir

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -2,5 +2,6 @@
 
 # Create the directory that will contain Chef configuration and keys
 mkdir /etc/chef
-
+chown root:root /etc/chef
+chmod 755 /etc/chef
 curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v 12.4.1
