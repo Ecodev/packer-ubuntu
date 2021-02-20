0442fa3bcc6d5db3aa8e9dc9f60f3b0ee37e806e
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-02-20 14:27:21 +0900

    Sync packages with chef-base #7907

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -4,4 +4,33 @@
 
 apt-get -y update
 
-apt-get -y install unattended-upgrades unzip curl finger whois sysstat rsync screen mc tig vim make acl bash mosh tcpdump keychain telnet wget subversion htop git
+# This should be kept in sync with chef-base/attributes/default.rb
+apt-get -y install \
+acl \
+bash \
+cmake \
+curl \
+finger \
+git \
+gnupg \
+htop \
+keychain \
+logrotate \
+make \
+mc \
+mosh \
+net-tools \
+rsync \
+ruby2.7 \
+ruby2.7-dev \
+screen \
+subversion \
+sysstat \
+tcpdump \
+telnet \
+tig \
+unattended-upgrades \
+unzip \
+vim \
+wget \
+whois
